c35b71ef2c91fdf30bfabda2b242dfbdf68c36c4
Jabin <151993918+jabinfoxiom@users.noreply.github.com>
2025-05-20 12:53:41 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the hostname
+HOSTNAME=$(hostname)
+
+# Get the unique machine ID
+MACHINE_ID=$(cat /etc/machine-id)
+
+# Get the public IP address
+PUBLIC_IP=$(curl -s http://ifconfig.me)
+
+# Create JSON object
+JSON=$(jq -n \
+  --arg hostname "$HOSTNAME" \
+  --arg machine_id "$MACHINE_ID" \
+  --arg public_ip "$PUBLIC_IP" \
+  '{hostname: $hostname, machine_id: $machine_id, public_ip: $public_ip}')
+
+# Output the JSON
+echo "$JSON"
