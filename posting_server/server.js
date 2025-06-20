186eec344b6ef66329a82e7e37307dbd3ddc022f
Jabin <jabinfoxiom@gmail.com>
2025-06-20 14:47:54 +0530

    minor fixes

--- posting_server/server.js
+++ posting_server/server.js
@@ -6,9 +6,6 @@ const { collectCPUMetrics } = require("./utils/cpuMetrics");
 const { collectMemoryMetrics } = require("./utils/memoryMetrics");
 const { collectDiskMetrics } = require("./utils/diskMetrics");
 const { collectNetworkMetrics } = require("./utils/networkMetrics");
-const authRoutes = require("./routes/auth");
-// const User = require('./models/User');
-// const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000;
@@ -17,34 +14,6 @@ const intervalInSeconds = 60;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-
-// async function initializeDefaultAdmin() {
-//     try {
-//         const defaultAdminEmail = 'superadmin@gmail.com';
-//         const defaultAdminPassword = '12345';
-
-//         const existingAdmin = await User.findOne({ email: defaultAdminEmail });
-//         if (existingAdmin) {
-//             console.log('Default admin user already exists');
-//             return;
-//         }
-
-//         const salt = await bcrypt.genSalt(10);
-//         const hashedPassword = await bcrypt.hash(defaultAdminPassword, salt);
-
-//         const adminUser = new User({
-//             email: defaultAdminEmail,
-//             password: hashedPassword,
-//             role: 'admin'
-//         });
-
-//         await adminUser.save();
-//         console.log('Default admin user created successfully');
-//     } catch (error) {
-//         console.error('Error creating default admin user:', error);
-//     }
-// }
 
 async function collectDeviceDetails() {
   try {
@@ -60,7 +29,6 @@ async function collectDeviceDetails() {
 app.listen(port, async () => {
   console.log(`Posting server is listening on port ${port}`);
   await connectToMongoDB();
-  // await initializeDefaultAdmin();
   await sendDeviceDetails();
   collectDeviceDetails();
   setInterval(collectDeviceDetails, intervalInSeconds * 1000);
